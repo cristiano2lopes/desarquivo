1611c81af221ec976d3b6091b0aa457fb4f1e14c
Cristiano Lopes <e.mail@cristianolopes.eu>
2023-04-19 00:06:25 +0100

    Add ojogo extractor

--- extractor/ojogo.py
+++ extractor/ojogo.py
@@ -24,16 +24,77 @@ from extractor.core import (
 logger = logging.getLogger(__name__)
 
 
-def extract_news_highlight_XXX(content) -> [ExtractionResult]:
+def extract_news_highlight_2014(content) -> [ExtractionResult]:
     """Extracts_news_highlight from layout in this example
+    https://arquivo.pt/wayback/20140427170213/http://www.ojogo.pt/
     """
-    return []
+    d = pq(content)
+    results = []
+
+    table = d(".LinkList_3 .LLItem")
+    if table:
+        for item in table[:3]:
+            item_pq = pq(item)
+            title = item_pq("h2").text()
+            link = item_pq("a").attr("href")
+            summary = item_pq(".LLIBLead").text()
+
+            if title and link:
+                results.append(
+                    ExtractionResult(
+                        content=NewsHighlight(
+                            **{
+                                "title": title,
+                                "summary": summary,
+                            }
+                        ),
+                        accessory_content=NewsHighlightAccessory(
+                            **{
+                                "more_link": link,
+                            }
+                        ),
+                    )
+                )
+    return results
+
+
+def extract_news_highlight_2017(content) -> [ExtractionResult]:
+    """Extracts_news_highlight from layout in this example
+    https://arquivo.pt/noFrame/replay/20170101180216/http://www.ojogo.pt/
+    """
+    d = pq(content)
+    results = []
+
+    table = d(".t-g1-featured-1")
+    if table:
+        title = table("h2 a").text()
+        link = table("h2 a").attr("href")
+        summary = ""
+
+        if title and link:
+            results.append(
+                ExtractionResult(
+                    content=NewsHighlight(
+                        **{
+                            "title": title,
+                            "summary": summary,
+                        }
+                    ),
+                    accessory_content=NewsHighlightAccessory(
+                        **{
+                            "more_link": link,
+                        }
+                    ),
+                )
+            )
+    return results
 
 
 class OJOGOV1(Extractor):
     version = "v1"
     urls = [
-        ExtractionTargetURL("http://www.ojogo.pt/", 1998, 2022),
+        ExtractionTargetURL("http://www.ojogo.pt", 2003, 2022),
+        ExtractionTargetURL("http://www.ojogo.pt/index.asp", 2003, 2022),
     ]
 
     def extract_news_highlight(
@@ -49,7 +110,8 @@ class OJOGOV1(Extractor):
 
         if dt and content:
             results = itertools.chain(
-                extract_news_highlight_XXX(archived_url.content),
+                extract_news_highlight_2014(archived_url.content),
+                extract_news_highlight_2017(archived_url.content),
             )
 
             for result in results:
